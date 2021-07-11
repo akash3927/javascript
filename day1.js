d2d404d8bc34a3e8c379c2e0668a513182f2a028
akash3927 <akashkothawade77@gmail.com>
2021-07-11 23:15:32 +0530

    day1

---
+++ day1.js
@@ -0,0 +1,11 @@
+function add(a, b) {
+  console.log(a + b);
+}
+add(6, 7);
+
+console.log("hello world!");
+// take the input from the user
+const number = prompt("Enter the number: ");
+
+const result = Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);
