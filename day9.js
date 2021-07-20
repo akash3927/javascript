c1ec4991d8d3c7fbca484e01af6dca4c31b3b905
akash3927 <akashkothawade77@gmail.com>
2021-07-20 22:32:03 +0530

    git push origin main

---
+++ day9.js
@@ -0,0 +1,37 @@
+// program to write to console
+
+// passing number
+console.log(8);
+
+// passing string
+console.log("hello");
+
+// passing variable
+const x = "hello";
+console.log(x);
+
+// passing function
+function sayName() {
+  return "Hello John";
+}
+console.log(sayName());
+
+// passing string and a variable
+const name = "John";
+console.log("Hello " + name);
+
+// passing object
+let obj = {
+  name: "John",
+  age: 28,
+};
+console.log(obj);
+
+// program to convert date to number
+// create date
+const d1 = new Date();
+console.log(d1);
+
+// converting to number
+const result = d1.getTime();
+console.log(result);
