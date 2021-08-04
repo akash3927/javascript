a4307725dd61b7a40674542c99a03863189de0c0
akash3927 <akashkothawade77@gmail.com>
2021-08-04 22:35:03 +0530

    new commit

---
+++ day22.js
@@ -0,0 +1,27 @@
+// program to split array into smaller chunks
+
+function splitIntoChunk(arr, chunk) {
+  for (i = 0; i < arr.length; i += chunk) {
+    let tempArray;
+    tempArray = arr.slice(i, i + chunk);
+    console.log(tempArray);
+  }
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8];
+const chunk = 2;
+splitIntoChunk(array, chunk);
+
+// program to split array into smaller chunks
+
+function splitIntoChunk(arr, chunk) {
+  while (arr.length > 0) {
+    let tempArray;
+    tempArray = arr.splice(0, chunk);
+    console.log(tempArray);
+  }
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8];
+const chunk = 2;
+splitIntoChunk(array, chunk);
