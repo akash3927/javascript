796f97e14d80c2e71a2005136518064058fc9c67
akash3927 <akashkothawade77@gmail.com>
2021-08-17 23:14:56 +0530

    new commit

---
+++ day34.js
@@ -0,0 +1,20 @@
+// program to check if an object is an array
+
+function checkObject(arr) {
+
+    // check if arr is array
+    const result = Array.isArray(arr);
+
+    if(result) {
+        console.log(`[${arr}] is an array.`);
+    }
+    else {
+        console.log(`${arr} is not an array.`);
+    }
+
+}
+
+const array = [1, 2, 3];
+
+// call the function
+checkObject(array);
