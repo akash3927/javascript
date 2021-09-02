30807523ff31342c24d8acba789e50fcc389c9e1
akash3927 <akashkothawade77@gmail.com>
2021-09-02 23:01:51 +0530

    new commit

---
+++ day47.js
@@ -0,0 +1,46 @@
+// js program to perform string comparison
+
+const string1 = 'JavaScript Program';
+const string2 = 'javascript program';
+
+// compare both strings
+const result = string1.toUpperCase() === string2.toUpperCase();
+
+if(result) {
+    console.log('The strings are similar.');
+} else {
+    console.log('The strings are not similar.');
+}
+
+
+
+// program to perform string comparison
+
+const string1 = 'JavaScript Program';
+const string2 = 'javascript program';
+
+// create regex
+const pattern = new RegExp(string1, "gi");
+
+// compare the stings
+const result = pattern.test(string2)
+
+if(result) {
+    console.log('The strings are similar.');
+} else {
+    console.log('The strings are not similar.');
+}
+
+
+// program to perform case insensitive string comparison
+
+const string1 = 'JavaScript Program';
+const string2 = 'javascript program';
+
+const result = string1.localeCompare(string2, undefined, { sensitivity: 'base' });
+
+if(result == 0) {
+    console.log('The strings are similar.');
+} else {
+    console.log('The strings are not similar.');
+}
