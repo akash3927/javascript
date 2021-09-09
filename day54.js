9bda44ff604427552aba0cbd7100634225b8851d
Akash <akashkothawade77@gmail.com>
2021-09-09 23:05:38 +0530

    new commit

---
+++ day54.js
@@ -0,0 +1,31 @@
+/** @format */
+
+// program to create a multiline strings
+
+// using the + operator
+const message =
+	'This is a long message\n' +
+	'that spans across multiple lines\n' +
+	'in the code.';
+
+console.log(message);
+
+// program to create a multiline strings
+
+// using the \ operator
+const message =
+	'This is a long message\n \
+that spans across multiple lines\n \
+in the code.';
+
+console.log(message);
+
+// program to create a multiline strings
+
+// using the template literal
+
+const message = `This is a long message
+that spans across multiple lines
+in the code.`;
+
+console.log(message);
