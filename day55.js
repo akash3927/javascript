41c6fbd7546a693c48db58d495b0848e83764c40
akash3927 <akashkothawade77@gmail.com>
2021-09-10 22:33:28 +0530

    new commit

---
+++ day55.js
@@ -0,0 +1,27 @@
+// program to add a key/value pair to an object
+
+const person = {
+    name: 'Monica',
+    age: 22,
+    gender: 'female'
+}
+
+// add a key/value pair
+person.height = 5.4;
+
+console.log(person);
+
+
+
+// program to add a key/value pair to an object
+
+const person = {
+    name: 'Monica',
+    age: 22,
+    gender: 'female'
+}
+
+// add a key/value pair
+person['height'] = 5.4;
+
+console.log(person);
