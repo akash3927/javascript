782f7a85b108148f646f523be0530dab1df728f1
Akash <akashkothawade77@gmail.com>
2021-09-12 23:04:50 +0530

    new commit

---
+++ day57.js
@@ -0,0 +1,36 @@
+/** @format */
+
+// program to merge property of two objects
+
+// object 1
+const person = {
+	name: 'Jack',
+	age: 26,
+};
+
+// object 2
+const student = {
+	gender: 'male',
+};
+
+// merge two objects
+const newObj = Object.assign(person, student);
+
+console.log(newObj);
+// program to merge property of two objects
+
+// object 1
+const person = {
+	name: 'Jack',
+	age: 26,
+};
+
+// object 2
+const student = {
+	gender: 'male',
+};
+
+// merge two objects
+const newObj = { ...person, ...student };
+
+console.log(newObj);
