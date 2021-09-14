f7699232c04cfda60e13a03b7c74b5ac749d5ca4
Akash <akashkothawade77@gmail.com>
2021-09-14 21:05:44 +0530

    new commit

---
+++ day59.js
@@ -0,0 +1,56 @@
+/** @format */
+
+// program to clone the object
+
+// declaring object
+const person = {
+	name: 'John',
+	age: 21,
+};
+
+// cloning the object
+const clonePerson = Object.assign({}, person);
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+
+// program to clone the object
+// declaring object
+const person = {
+	name: 'John',
+	age: 21,
+};
+
+// cloning the object
+const clonePerson = { ...person };
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+
+// program to clone the object
+// declaring object
+const person = {
+	name: 'John',
+	age: 21,
+};
+
+// cloning the object
+const clonePerson = JSON.parse(JSON.stringify(person));
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
