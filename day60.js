9210cf2f759c9ff673d33b7e9fce2fcee556e6ec
Akash <akashkothawade77@gmail.com>
2021-09-15 23:00:33 +0530

    new commit

---
+++ day60.js
@@ -0,0 +1,34 @@
+/** @format */
+
+// program to check if a key exists
+
+const person = {
+	id: 1,
+	name: 'John',
+	age: 23,
+};
+
+// check if key exists
+const hasKey = 'name' in person;
+
+if (hasKey) {
+	console.log('The key exists.');
+} else {
+	console.log('The key does not exist.');
+}
+// program to check if a key exists
+
+const person = {
+	id: 1,
+	name: 'John',
+	age: 23,
+};
+
+//check if key exists
+const hasKey = person.hasOwnProperty('name');
+
+if (hasKey) {
+	console.log('The key exists.');
+} else {
+	console.log('The key does not exist.');
+}
