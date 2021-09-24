157c6084910b19790a7e3d61e7443f5fed24379b
Akash <akashkothawade77@gmail.com>
2021-09-24 22:17:50 +0530

    new commit

---
+++ day69.js
@@ -0,0 +1,49 @@
+/** @format */
+
+// program to check if the string is palindrome or not
+
+function checkPalindrome(str) {
+	// find the length of a string
+	const len = string.length;
+
+	// loop through half of the string
+	for (let i = 0; i < len / 2; i++) {
+		// check if first and last string are same
+		if (string[i] !== string[len - 1 - i]) {
+			return 'It is not a palindrome';
+		}
+	}
+	return 'It is a palindrome';
+}
+
+// take input
+const string = prompt('Enter a string: ');
+
+// call the function
+const value = checkPalindrome(string);
+
+console.log(value);
+
+// program to check if the string is palindrome or not
+
+function checkPalindrome(str) {
+	// convert string to an array
+	const arrayValues = string.split('');
+
+	// reverse the array values
+	const reverseArrayValues = arrayValues.reverse();
+
+	// convert array to string
+	const reverseString = reverseArrayValues.join('');
+
+	if (string == reverseString) {
+		console.log('It is a palindrome');
+	} else {
+		console.log('It is not a palindrome');
+	}
+}
+
+//take input
+const string = prompt('Enter a string: ');
+
+checkPalindrome(string);
