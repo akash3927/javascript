c1b0e35fb6146154429b397ab62b58c1e6346e40
Akash <akashkothawade77@gmail.com>
2021-09-25 21:48:40 +0530

    new commit

---
+++ day70.js
@@ -0,0 +1,35 @@
+/** @format */
+
+// program to convert decimal to binary
+function convertToBinary(x) {
+	let bin = 0;
+	let rem,
+		i = 1,
+		step = 1;
+	while (x != 0) {
+		rem = x % 2;
+		console.log(
+			`Step ${step++}: ${x}/2, Remainder = ${rem}, Quotient = ${parseInt(
+				x / 2,
+			)}`,
+		);
+		x = parseInt(x / 2);
+		bin = bin + rem * i;
+		i = i * 10;
+	}
+	console.log(`Binary: ${bin}`);
+}
+
+// take input
+let number = prompt('Enter a decimal number: ');
+
+convertToBinary(number);
+// program to convert decimal to binary
+
+// take input
+const number = parseInt(prompt('Enter a decimal number: '));
+
+// convert to binary
+const result = number.toString(2);
+
+console.log('Binary:' + ' ' + result);
