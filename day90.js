f3c7868cf7e4323479e5beedb107cdb8d8d93c83
Akash <akashkothawade77@gmail.com>
2021-10-17 23:43:40 +0530

    added microtasks working

---
+++ day90.js
@@ -0,0 +1,17 @@
+/** @format */
+
+console.log('script start');
+
+setTimeout(function () {
+	console.log('setTimeout');
+}, 0);
+
+Promise.resolve()
+	.then(function () {
+		console.log('promise1');
+	})
+	.then(function () {
+		console.log('promise2');
+	});
+
+console.log('script end');
