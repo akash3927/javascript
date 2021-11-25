455ac203afed620ddf4fc92bd3ca879c8736c96b
Akash <akashkothawade77@gmail.com>
2021-11-25 21:50:50 +0530

    anonymous function

---
+++ anonymous.js
@@ -0,0 +1,14 @@
+/** @format */
+
+//An anonymous function is a function without a name! Anonymous functions are commonly assigned to a variable name or used as a callback function. The syntax would be as below,
+function(optionalParameters) {
+  //do something
+}
+
+const myFunction = function(){ //Anonymous function assigned to a variable
+  //do something
+};
+
+[1, 2, 3].map(function(element){ //Anonymous function used as a callback function
+  //do something
+});
