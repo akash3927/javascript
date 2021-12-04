22123dfeded95b1f29e20e19d5d581f91aeffef7
Akash <akashkothawade77@gmail.com>
2021-12-04 21:56:42 +0530

    new commit

---
+++ firstorder.js
@@ -0,0 +1,4 @@
+/** @format */
+
+//First-order function is a function that doesn’t accept another function as an argument and doesn’t return a function as its return value.
+const firstOrder = () => console.log('I am a first order function!');
